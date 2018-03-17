290c948ac111e44a3872ebf430a02938f46cea54
Sanjeev Chauhan <Sanjeev.Chauhan@e2open.com>
2018-03-17 22:01:14 +0530

    Base code for react application

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const DIST_DIR = path.join(__dirname, "dist");
+const SRC_DIR = path.join(__dirname, "src");
+
+module.exports = {
+  entry: path.join(SRC_DIR, "index.js"),
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: SRC_DIR,
+        loader: ["babel-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  devServer: {
+    contentBase: DIST_DIR,
+    hot: true
+  }
+};
+
+
